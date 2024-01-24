3d49db1a8aef6089508bc3aec7f505d4d9ddf97e
PotatoPTT <41968475+PotatoPTT@users.noreply.github.com>
2024-01-23 22:39:32 -0300

    first commit

---
+++ xcloud-notify.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         xCloud Notification
+// @namespace    https://github.com/redphx
+// @version      1.0.0
+// @description  Simple Notification for Xcloud
+// @author       PotatoPTT
+// @license      MIT
+// @match        https://www.xbox.com/*/play*
+// @run-at       document-start
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/PotatoPTT/XCloudNotify/main/xcloud-notify.meta.js
+// @downloadURL  https://raw.githubusercontent.com/PotatoPTT/XCloudNotify/main/xcloud-notify.js
+// ==/UserScript==
+'use strict';
+
+function verifyNotification() {
+    const originalFetch = window.fetch;
+    window.fetch = async (...args) => {
+        let request = args[0];
+        let url = (typeof request === 'string') ? request : request.url;
+        if (url.endsWith('/configuration') && url.includes('/sessions/cloud/') && request.method === 'GET') {
+            console.log("Notification Sent!");
+            new Notification('Game is Ready', {
+                body: 'Queue finished.'
+            });
+        }
+
+        return originalFetch(...args);
+    };
+}
+
+verifyNotification();
---
+++ xcloud-notify.meta.js
@@ -0,0 +1,5 @@
+// ==UserScript==
+// @name         xCloud Notification
+// @namespace    https://github.com/PotatoPTT
+// @version      1.0.0
+// ==/UserScript==
